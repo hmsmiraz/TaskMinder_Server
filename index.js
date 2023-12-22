ecb0547afcce6433fb2153d070a2d3239e65ff72
hmsmiraz <hmsmiraz64729@gmail.com>
2023-12-22 16:32:25 +0600

    done

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.meftkqt.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -44,7 +44,34 @@ async function run() {
       res.send(result);
     });
 
-    
+    // task api
+    app.get("/tasks",  async (req, res) => {
+      const cursor = taskCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.post("/tasks", async (req, res) => {
+      const blogs = req.body;
+      const result = await taskCollection.insertOne(blogs);
+      res.send(result);
+    });
+
+    app.get("/tasks/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await taskCollection.findOne(query);
+      //console.log(result);
+      res.send(result);
+    });
+
+    app.delete("/tasks/:id",  async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await taskCollection.deleteOne(query);
+      res.send(result);
+    });
+
     
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
