1e87c6d73e9ff59dd5b51ac6098ea7687c8cb368
hmsmiraz <hmsmiraz64729@gmail.com>
2024-01-13 20:01:38 +0600

    added task

--- index.js
+++ index.js
@@ -3,13 +3,28 @@ const app = express();
 const cors = require("cors");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
+const fs = require('fs');
 
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" }); 
 // middleware
 app.use(cors());
 app.use(express.json());
 
+const createUploadsFolder = () => {
+  const folderPath = './uploads';
+  if (!fs.existsSync(folderPath)) {
+    fs.mkdirSync(folderPath);
+    console.log('Uploads folder created successfully.');
+  } else {
+    console.log('Uploads folder already exists.');
+  }
+};
 
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+// Call the function to create the "uploads" folder
+createUploadsFolder();
+
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.meftkqt.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -18,42 +33,58 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
+    
 
     // collections
     const userCollection = client.db("taskMinderDB").collection("users");
     const taskCollection = client.db("taskMinderDB").collection("tasks");
 
     // users
-    app.get("/users",  async (req, res) => {
+    app.get("/users", async (req, res) => {
       const cursor = userCollection.find();
       const users = await cursor.toArray();
       res.send(users);
     });
 
-    app.post("/users",  async (req, res) => {
+    app.post("/users", async (req, res) => {
       const user = req.body;
-      //console.log(user);
       const result = await userCollection.insertOne(user);
       res.send(result);
     });
 
     // task api
-    app.get("/tasks",  async (req, res) => {
+    app.get("/tasks", async (req, res) => {
       const cursor = taskCollection.find();
       const result = await cursor.toArray();
       res.send(result);
     });
 
-    app.post("/tasks", async (req, res) => {
-      const blogs = req.body;
-      const result = await taskCollection.insertOne(blogs);
+    // app.post("/tasks", async (req, res) => {
+    //   const taskData = req.body;
+    //   const result = await taskCollection.insertOne(taskData);
+    //   res.send(result);
+    // });
+
+
+    app.post("/tasks", upload.array("files"), async (req, res) => {
+      const taskData = req.body;
+      if (req.files && req.files.length > 0) {
+        taskData.files = req.files.map((file) => ({
+          filename: file.filename,
+          originalname: file.originalname
+        }));
+      } else {
+        taskData.files = [];
+      }
+    
+      const result = await taskCollection.insertOne(taskData);
       res.send(result);
     });
 
@@ -61,21 +92,21 @@ async function run() {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await taskCollection.findOne(query);
-      //console.log(result);
       res.send(result);
     });
 
-    app.delete("/tasks/:id",  async (req, res) => {
+    app.delete("/tasks/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await taskCollection.deleteOne(query);
       res.send(result);
     });
 
-    
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -83,11 +114,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
 app.get("/", (req, res) => {
-    res.send("TaskMinder Server is Running...");
-  });
-  
-  app.listen(port, () => {
-    console.log(`TaskMinder Server is Running on port ${port}`);
-  })
+  res.send("TaskMinder Server is Running...");
+});
+
+app.listen(port, () => {
+  console.log(`TaskMinder Server is Running on port ${port}`);
+});
